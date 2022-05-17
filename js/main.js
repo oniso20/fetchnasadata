7c6939f9384721a39ec966ad16e85761a0c80d9a
Onis Emem <emem.c.onis@gmail.com>
2022-05-17 20:45:10 +0300

    Displaying the data as a line graph

--- js/main.js
+++ js/main.js
@@ -1,6 +1,31 @@
 // Data from https://data.giss.nasa.gov/gistemp/
 
-getNasaData()
+const xLabels = [];
+const yTemps = [];
+
+chartIt();
+
+
+async function chartIt() {
+    await getNasaData();
+    const ctx = document.getElementById('chart').getContext('2d');
+    
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xLabels,
+            datasets: [{
+                label: 'Combined Land-Surface Air and Sea-Surface Water Temperature C° (Zonal annual means) ',
+                data: yTemps,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                fill: true,
+                borderWidth: 1
+            }]
+        }
+    });
+
+}
 
 async function getNasaData() {
   const response = await fetch('ZonAnn.Ts+dSST.csv');
@@ -15,7 +40,12 @@ async function getNasaData() {
     const column = row.split(',');
     //Getting the column index 
     const temp = column[1];
+    // Pushing the temp data to the y-axis
+    //The global temperature mean is 14 degrees centigrate.. hence temp + 14 but temp must be parsed as a float not text using parseFloat()
+    yTemps.push(parseFloat(temp) + 14) 
     const year = column[0];
+    //Push year into the x axis label - xLabels
+    xLabels.push(year);
     console.log(year, temp)
   })
 }
